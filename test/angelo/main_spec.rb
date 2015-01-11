8181fe0df67a351eee1c24e61e23557592b3199d
Tom May <tom@tommay.net>
2015-01-11 14:47:04 -0800

    Add more option parsing tests.

--- test/angelo/main_spec.rb
+++ test/angelo/main_spec.rb
@@ -2,8 +2,8 @@ require_relative '../spec_helper'
 require 'angelo/main'
 
 describe "angelo/main" do
-  describe "parses" do
-    def with_args(args, &block)
+  helpers = Module.new do
+    def self.with_args(args, &block)
       orig_argv = ARGV.dup
       begin
         ARGV.replace(args.split)
@@ -12,19 +12,37 @@ describe "angelo/main" do
         ARGV.replace(orig_argv)
       end
     end
+  end
 
+  describe "parses" do
     it "-p port" do
-      with_args("-p 12345") do
+      helpers.with_args("-p 12345") do
         klass = Class.new(Angelo::Base)
         klass.port.must_equal 12345
       end
     end
 
     it "-o addr" do
-      with_args("-o 3.2.1.0") do
+      helpers.with_args("-o 3.2.1.0") do
         klass = Class.new(Angelo::Base)
         klass.addr.must_equal "3.2.1.0"
       end
+    end
+  end
+
+  it "uses defaults if not specified" do
+    helpers.with_args("") do
+      klass = Class.new(Angelo::Base)
+      klass.port.must_equal Angelo::DEFAULT_PORT
+      klass.addr.must_equal Angelo::DEFAULT_ADDR
+    end
+  end
+
+  it "doesn't alter ARGV" do
+    helpers.with_args("-p 1234 -o 109.87.65.43") do
+      pre_args = ARGV.dup
+      Class.new(Angelo::Base)
+      ARGV.must_equal pre_args
     end
   end
 end
