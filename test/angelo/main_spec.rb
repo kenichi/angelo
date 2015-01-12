ae1dcb43e27bb48bd2e9b6206014b2169a2fdc25
Tom May <tom@tommay.net>
2015-01-12 01:24:16 -0800

    Don't need instance_eval since Class.new already takes a block.

--- test/angelo/main_spec.rb
+++ test/angelo/main_spec.rb
@@ -4,14 +4,14 @@ require 'angelo/main'
 describe Angelo::Base do
   describe "parses" do
     it "-p port" do
-      Class.new(Angelo::Base).instance_eval do
+      Class.new(Angelo::Base) do
         parse_options("-p 12345".split)
         port.must_equal 12345
       end
     end
 
     it "-o addr" do
-      Class.new(Angelo::Base).instance_eval do
+      Class.new(Angelo::Base) do
         parse_options("-o 3.2.1.0".split)
         addr.must_equal "3.2.1.0"
       end
