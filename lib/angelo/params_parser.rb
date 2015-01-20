deb7df83f066caa90197fd9c01e54b4b736f9f03
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2015-01-19 21:35:54 -0800

    going a bit further?

--- lib/angelo/params_parser.rb
+++ lib/angelo/params_parser.rb
@@ -53,4 +53,19 @@ module Angelo
 
   end
 
+  class SymHash < Hash
+
+    # Returns a Hash that allows values to be fetched with String or
+    # Symbol keys.
+    def initialize h = nil
+      super(){|hash,key| hash[key.to_s] if Symbol === key}
+      unless h.nil?
+        merge! h
+        # Replace values that are Hashes with SymHashes, recursively.
+        each {|k,v| self[k] = SymHash.new(v) if Hash === v}
+      end
+    end
+
+  end
+
 end
--- test/angelo/params_spec.rb
+++ test/angelo/params_spec.rb
@@ -97,3 +97,20 @@ describe Angelo::ParamsParser do
   end
 
 end
+
+describe Angelo::SymHash do
+
+  describe ".new" do
+    it "returns a Hash" do
+      Angelo::SymHash.new.must_be_kind_of Hash
+    end
+  end
+
+  it "fetches values for Symbols that were inserted with Strings" do
+    symhash = Angelo::SymHash.new
+    symhash["x"] = "y"
+    symhash["x"].must_equal "y"
+    symhash[:x].must_equal "y"
+  end
+
+end
