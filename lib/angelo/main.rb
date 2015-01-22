99a2e51d985ac19a00ad86d2180b39ebca99d5e4
Tom May <tom@tommay.net>
2015-01-22 08:45:37 -0800

    Run @angelo_app with at_exit.

--- lib/angelo/main.rb
+++ lib/angelo/main.rb
@@ -35,4 +35,13 @@ if self.to_s == "main"
   self.extend Forwardable
   @angelo_app = Class.new(Angelo::Base)
   self.def_delegators :@angelo_app, *Angelo::Base::DSL.instance_methods
+
+  at_exit do
+    # Don't run @angelo_app on uncaught exceptions including exit
+    # being called which raises SystemExit.  The rationale being that
+    # exit means exit, not "run the server".
+    if $!.nil?
+      @angelo_app.run!
+    end
+  end
 end
